6b5653f6e757928c9aa05b10828706c6529b31c0
darlison-calm <bernardo.darlison@gmail.com>
2025-03-17 16:49:42 -0300

    feat: create PetAddress class

---
+++ src/entities/pet/PetAddress.java
@@ -0,0 +1,37 @@
+package entities.pet;
+
+public class PetAddress {
+    private int houseNumber;
+    private String city;
+    private String street;
+
+    public PetAddress(int houseNumber, String city, String street) {
+        this.houseNumber = houseNumber;
+        this.city = city;
+        this.street = street;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+}
