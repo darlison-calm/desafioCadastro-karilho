7d5472d6f1bbb8719ec62d99131a534c29010006
darlison-calm <bernardo.darlison@gmail.com>
2025-03-17 16:54:49 -0300

    feat: validate pet type and rename name validation function - Introduced the isValidPetType function to verify if the user-provided pet type is within the allowed list. - Renamed the validatePetName function to isValidPetName for improved clarity and consistency in naming conventions.

--- src/entities/pet/PetInputValidator.java
+++ src/entities/pet/PetInputValidator.java
@@ -1,11 +1,19 @@
 package entities.pet;
 
+import enums.PetType;
+
 public class PetInputValidator {
 
-    public static boolean validatePetName(String name) {
+    public static boolean isValidPetName(String name) {
         if (name == null) {
             return false;
         }
         return name.trim().matches("[A-Za-z]+ [A-Za-z]+");
     }
+
+    public static boolean isValidPetType(String petType) {
+        return petType.equalsIgnoreCase(PetType.CAT.getDescription()) ||
+                petType.equalsIgnoreCase(PetType.DOG.getDescription());
+    }
+
 }
